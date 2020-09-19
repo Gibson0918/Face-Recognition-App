771260f52a87f175e44ba5bca8236d080e1201d4
Gibson <gibsonyip@gmail.com>
2020-09-19 19:05:29 +0800

    Completed Face Recognition, will have to clean up code tmr

--- app/src/main/java/com/example/cameraxxx/MainActivity.java
+++ app/src/main/java/com/example/cameraxxx/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.CameraX;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.extensions.HdrImageCaptureExtender;
 import androidx.camera.lifecycle.ProcessCameraProvider;
@@ -15,19 +17,39 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
+import android.util.Size;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+import com.google.firebase.ml.vision.common.FirebaseVisionPoint;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceContour;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceLandmark;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
@@ -50,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
         mPreviewView = findViewById(R.id.previewView);
         captureImage = findViewById(R.id.captureImg);
 
+        //check if permission is granted
         if(allPermissionsGranted()){
             startCamera(); //start camera if permission has been granted by user
         } else{
@@ -57,10 +80,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @SuppressLint("RestrictedApi")
+    protected void onDestroy(){
+        super.onDestroy();
+        CameraX.unbindAll();
+    }
+
     private void startCamera() {
 
+        //request for a CameraProvider
         final ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
 
+        //Check for CameraProvider's Availability --> verify initialization succeeded
         cameraProviderFuture.addListener(new Runnable() {
             @Override
             public void run() {
@@ -78,7 +109,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
-
         Preview preview = new Preview.Builder()
                 .build();
 
@@ -86,6 +116,121 @@ public class MainActivity extends AppCompatActivity {
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
                 .build();
 
+        ImageAnalysis imageAnalysis =
+                new ImageAnalysis.Builder()
+                        .setTargetResolution(new Size(1280, 720))
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                        .build();
+
+
+        imageAnalysis.setAnalyzer(executor, new ImageAnalysis.Analyzer() {
+            @SuppressLint("UnsafeExperimentalUsageError")
+            @Override
+            public void analyze(@NonNull ImageProxy imageProxy) {
+                int rotationDegrees = imageProxy.getImageInfo().getRotationDegrees();
+
+                FirebaseVisionFaceDetectorOptions realTimeOpts =
+                        new FirebaseVisionFaceDetectorOptions.Builder()
+                                .setContourMode(FirebaseVisionFaceDetectorOptions.ALL_CONTOURS)
+                                .build();
+                /*if (imageProxy == null || imageProxy.getImage() == null) {
+                    return;
+                }*/
+                Image mediaImage = imageProxy.getImage();
+                int rotation = degreesToFirebaseRotation(rotationDegrees);
+                FirebaseVisionImage image =
+                        FirebaseVisionImage.fromMediaImage(mediaImage, rotation);
+                Bitmap bitmapFactory = image.getBitmap();
+
+                FirebaseVisionFaceDetector detector = FirebaseVision.getInstance()
+                        .getVisionFaceDetector(realTimeOpts);
+
+                Task<List<FirebaseVisionFace>> result =
+                        detector.detectInImage(image)
+                                .addOnSuccessListener(
+                                        new OnSuccessListener<List<FirebaseVisionFace>>() {
+                                            @Override
+                                            public void onSuccess(List<FirebaseVisionFace> faces) {
+                                                 for (FirebaseVisionFace face : faces) {
+                                                    Rect bounds = face.getBoundingBox();
+                                                    float rotY = face.getHeadEulerAngleY();  // Head is rotated to the right rotY degrees
+                                                    float rotZ = face.getHeadEulerAngleZ();  // Head is tilted sideways rotZ degrees
+                                                     Toast.makeText(MainActivity.this, "Face recognized", Toast.LENGTH_LONG).show();
+                                                    // If landmark detection was enabled (mouth, ears, eyes, cheeks, and
+                                                    // nose available):
+                                                    FirebaseVisionFaceLandmark leftEar = face.getLandmark(FirebaseVisionFaceLandmark.LEFT_EAR);
+                                                    if (leftEar != null) {
+                                                        FirebaseVisionPoint leftEarPos = leftEar.getPosition();
+                                                    }
+
+                                                    // If contour detection was enabled:
+                                                    List<FirebaseVisionPoint> leftEyeContour =
+                                                            face.getContour(FirebaseVisionFaceContour.LEFT_EYE).getPoints();
+                                                    List<FirebaseVisionPoint> upperLipBottomContour =
+                                                            face.getContour(FirebaseVisionFaceContour.UPPER_LIP_BOTTOM).getPoints();
+
+                                                    // If classification was enabled:
+                                                    if (face.getSmilingProbability() != FirebaseVisionFace.UNCOMPUTED_PROBABILITY) {
+                                                        float smileProb = face.getSmilingProbability();
+                                                    }
+                                                    if (face.getRightEyeOpenProbability() != FirebaseVisionFace.UNCOMPUTED_PROBABILITY) {
+                                                        float rightEyeOpenProb = face.getRightEyeOpenProbability();
+                                                    }
+
+                                                    // If face tracking was enabled:
+                                                    if (face.getTrackingId() != FirebaseVisionFace.INVALID_ID) {
+                                                        int id = face.getTrackingId();
+                                                    }
+                                                }
+                                            }
+                                        })
+                                .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                // Task failed with an exception
+                                                // ...
+                                                Log.d("test124",e.toString());
+                                            }
+                                        });
+
+
+                imageProxy.close();
+            }
+        });
+
+
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, imageAnalysis, preview);
+
+        preview.setSurfaceProvider(mPreviewView.createSurfaceProvider());
+    }
+
+    private int degreesToFirebaseRotation(int degrees) {
+        switch (degrees) {
+            case 0:
+                return FirebaseVisionImageMetadata.ROTATION_0;
+            case 90:
+                return FirebaseVisionImageMetadata.ROTATION_90;
+            case 180:
+                return FirebaseVisionImageMetadata.ROTATION_180;
+            case 270:
+                return FirebaseVisionImageMetadata.ROTATION_270;
+            default:
+                throw new IllegalArgumentException(
+                        "Rotation must be 0, 90, 180, or 270.");
+        }
+    }
+
+    /*//Select a camera and bind the lifecycle and use case.
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+
+        //Preview Use Case
+        Preview preview = new Preview.Builder()
+                .build();
+        //Select which camera to use (for now we only use the back camera)
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+        //Image Analysis use case
         ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
                 .build();
 
@@ -130,7 +275,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         });
-    }
+    }*/
 
     public String getBatchDirectoryName() {
 
